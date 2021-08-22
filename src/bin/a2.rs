0808d66671d8ac54512a3550935b13a382156778
friendbear <friendbear@users.noreply.github.com>
2021-08-22 16:58:12 +0900

    sum function.

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -5,7 +5,16 @@
 //
 // Notes:
 // * Use a function to add two numbers together
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
+}
 // * Use a function to display the result
+fn disoplay_result(res: i32) {
+    println!("{:?}", res)
+}
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+    let result = sum(2 , 2);
+    disoplay_result(result)
+}
