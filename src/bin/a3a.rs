890a45203c3539de8d59faf46568134127cb5bd1
friendbear <friendbear@users.noreply.github.com>
2021-08-23 15:10:12 +0900

    If & Else(Part1)

--- src/bin/a3a.rs
+++ src/bin/a3a.rs
@@ -5,9 +5,20 @@
 // * When the variable is set to true, display "hello"
 // * When the variable is set to false, display "goodbye"
 //
+fn say(valiable: bool) {
+    if valiable {
+        println!("hello")
+    } else {
+        println!("goodby")
+    }
+}
 // Notes:
 // * Use a variable set to either true or false
 // * Use an if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    let my_bool = true;
+
+    say(my_bool);
+}
