c6a1948405fd86db780f3e2be0496592cdc85998
friendbear <friendbear@users.noreply.github.com>
2021-08-23 15:30:07 +0900

    If & Else (Part2)

--- src/bin/a3b.rs
+++ src/bin/a3b.rs
@@ -9,4 +9,13 @@
 // * Use an if..else if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    let my_integer = 6;
+    if my_integer > 5 {
+        println!("gt 5");
+    } else if my_integer < 5 {
+        println!("lt 5");
+    } else if my_integer == 5 {
+        println!("eq 5");
+    }
+}
