fbd267f4eabe745090bbe54c4ad3b1504e6999f6
friendbear <friendbear@users.noreply.github.com>
2021-08-26 18:18:01 +0900

    :art: Implementing Functionality

--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -9,5 +9,60 @@
 // * Use an enum for the box color
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
+enum Color {
+    Brown,
+    Red,
+}
 
-fn main() {}
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Brown => println!("brown"),
+            Color::Red => println!("red"),
+        }
+    }
+}
+struct Dimensions {
+    width: f64,
+    height: f64,
+    depth: f64,
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("width: {:?}", self.width);
+        println!("height: {:?}", self.height);
+        println!("depth: {:?}", self.depth);
+    }
+}
+struct ShippingBox {
+    weight: f64,
+    color: Color,
+    dimensions:  Dimensions,
+}
+
+impl ShippingBox {
+    fn new(weight: f64, color: Color, dimensions: Dimensions) -> Self {
+        Self {
+            weight,
+            color,
+            dimensions,
+        }
+    }
+    fn print(&self) {
+        self.color.print();
+        self.dimensions.print();
+        println!("weight: {:?}", self.weight)
+    }
+}
+
+fn main() {
+
+    let small_dimensions = Dimensions {
+        width: 1.0,
+        height: 2.0,
+        depth: 3.0,
+    };
+    let small_box = ShippingBox::new(5.0, Color::Red, small_dimensions);
+    small_box.print();
+}
