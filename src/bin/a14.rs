f542fc3090a10d587627af73670fe62aa8dfaee6
friendbear <friendbear@users.noreply.github.com>
2021-08-28 07:21:24 +0900

    :art: String

--- src/bin/a14.rs
+++ src/bin/a14.rs
@@ -10,5 +10,34 @@
 // * Iterate through the vector using a for..in loop
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
+struct Person {
+    name: String,
+    fav_color: String,
+    age: i32,
+}
+fn main() {
 
-fn main() {}
+    let persons = vec![
+        Person {
+            name: "hoge".to_owned(),
+            fav_color: String::from("green"),
+            age: 32,
+        },
+        Person {
+            name: "Anna".to_owned(),
+            fav_color: String::from("purple"),
+            age: 9,
+        },
+        Person {
+            name: "Katie".to_owned(),
+            fav_color: String::from("blue"),
+            age: 10,
+        }
+    ];
+
+    let under10_age_persons = persons.iter().filter(|&x| x.age <= 10);
+    for person in under10_age_persons {
+        println!("{:?}", person.age);
+    }
+
+}
