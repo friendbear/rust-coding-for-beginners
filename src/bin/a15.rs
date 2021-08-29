5987678bb1ab6a64deeded5f2c362056a5e2552c
friendbear <friendbear@users.noreply.github.com>
2021-08-29 18:36:20 +0900

    :art: Advanced Match

--- src/bin/a15.rs
+++ src/bin/a15.rs
@@ -10,5 +10,28 @@
 // * Use an enum for the tickets with data associated with each variant
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
+enum Ticket {
+    Backstage(f64, String),
+    Standard(f64),
+    Vip(f64, String),
+}
 
-fn main() {}
+fn main() {
+    let tickets = vec![
+        Ticket::Backstage(50.0, "Billy".to_owned()),
+        Ticket::Standard(15.0),
+        Ticket::Vip(30.0, "Amy".to_owned())
+    ];
+
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(price, holder) => {
+                println!("Holder: {:?}, price: {:?}", holder, price)
+            }
+            Ticket::Standard(price) => println!("price: {:?}", price),
+            Ticket::Vip(price, holder) => {
+                println!("Holder: {:?}, price: {:?}", holder, price)
+            }
+        }
+    }
+}
