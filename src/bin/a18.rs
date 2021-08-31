4d979cef02046051b311c22f5501f28db68ed336
friendbear <friendbear@users.noreply.github.com>
2021-08-31 17:03:06 +0900

    :art: Result

--- src/bin/a18.rs
+++ src/bin/a18.rs
@@ -10,5 +10,22 @@
 // * Use a function to determine if a customer can make a restricted purchase
 // * Return a result from the function
 // * The Err variant should detail the reason why they cannot make a purchase
+struct Customer {
+    age: u32,
+}
 
-fn main() {}
+fn try_purchase(customer: &Customer, f: fn(u32) -> bool) -> Result<(), String> {
+
+    if f(customer.age) {
+        Err("customer must be at least purchase age".to_owned())
+    } else {
+        Ok(())
+    }
+}
+fn main() {
+    let age_check = |age: u32| age < 21;
+
+    let ashley = Customer { age: 12 };
+    let purchased = try_purchase(&ashley, age_check);
+    println!("{:?}", purchased);
+}
