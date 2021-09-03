cf5d77043f33ebfe3c00a1ea9349a4cf7a13db97
friendbear <friendbear@users.noreply.github.com>
2021-09-03 13:33:10 +0900

    :art: Result & Question Mark Operator

--- src/bin/a18b.rs
+++ src/bin/a18b.rs
@@ -15,11 +15,57 @@
 //
 // Notes:
 // * Use an enum to represent all types of employees
+#[derive(Debug)]
+enum Position {
+    Maintenance,
+    Marketing,
+    Manager,
+    LineSupervisor,
+    RitchhenStaff,
+    AssemblyTech,
+}
+
+#[derive(Debug)]
+enum Status {
+    Active,
+    Terminated,
+}
 // * Use a struct to store the employee type and whether they are
 //   still employed
 // * Use a function that returns a Result to determine if the employee
 //   may enter the building
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
+#[derive(Debug)]
+struct Employee {
+    position: Position,
+    status: Status,
+}
 
-fn main() {}
+fn try_access(employee: &Employee) -> Result<(), String> {
+
+    match employee.status {
+        Status::Terminated => return Err("terminated".to_owned()),
+        _ => (),
+    }
+
+    match employee.position {
+        Position::Maintenance => Ok(()),
+        Position::Marketing => Ok(()),
+        Position::Manager => Ok(()),
+        _ => Err("invalid position".to_owned()),
+    }
+}
+fn main() {
+
+    let manager = Employee {
+        position: Position::Manager,
+        status: Status::Terminated,
+    };
+
+    if let Err(e) = try_access(&manager) {
+        println!("Access denied: {:?}", e);
+    } else {
+        println!("Access allowed: {:?}", &manager);
+    }
+}
