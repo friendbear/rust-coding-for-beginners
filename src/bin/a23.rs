6ea287ce24e4c451a360f2393936cd3625240003
friendbear <friendbear@users.noreply.github.com>
2021-09-08 11:35:27 +0900

    :white_check_mark: Option Conbinators
    Need Check

--- src/bin/a23.rs
+++ src/bin/a23.rs
@@ -1,3 +1,5 @@
+use std::ptr::eq;
+
 // Topic: Option combinators
 //
 // Requirements:
@@ -13,22 +15,28 @@ fn part_1() -> bool {
     // has an access level. The "admin" user does have
     // an access level.
     // Note: Use is_some or is_none.
-    maybe_access("admin")
+    maybe_access("admin").is_some()
 }
 
 fn part_2() -> Option<Access> {
     // "Root" is equivalent to Access::Admin, but it is
     // not listed in the maybe_access function.
     // Note: Use or_else and root().
-    maybe_access("root")
+    maybe_access("root").or_else(|| root())
 }
 
 fn part_3() -> Access {
     // "Alice" is not a listed user, so she will be a guest.
     // Note: Use unwrap_or_else.
-    maybe_access("Alice")
+    maybe_access("Alice").unwrap_or_else(|| Access::Guest)
 }
-
+//#![feature(arbitrary_enum_discriminant)] ❌
+//#[repr(u8)]
+// enum Access {
+//     Admin(i32) = 0, ❌
+//     User(i32) = 1,
+//     Guest(i32) = 2,
+// }
 #[derive(Debug, Eq, PartialEq)]
 enum Access {
     Admin,
