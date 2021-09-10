01918f545ccdd7ec8d5e8fbe45893881a23df9e5
friendbear <friendbear@users.noreply.github.com>
2021-09-10 14:26:27 +0900

    :white_check_mark: Demo: if..let..else enum.

--- src/bin/iflet.rs
+++ src/bin/iflet.rs
@@ -1,7 +1,20 @@
+enum Color {
+    Red,
+    Blue,
+    Green,
+}
+
 fn main() {
     let maybe_user = Some("T Kumagai");
 
     if let Some(user) = maybe_user {
         println!("user={:?}", user);
+    }
+
+    let color = Color::Blue;
+    if let Color::Red = color {
+        println!("its red!");
+    } else {
+        println!("its not red");
     }
 }
